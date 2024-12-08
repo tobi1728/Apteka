a4e4361150ad35fc43c5b9407322ad0f4db1cbc2
Łukasz Tobolski <tobi1728@gmail.com>
2024-12-08 14:08:38 +0100

    Added Custom Control WPF -> AllViewBase.cs and changed inheritance of AllViewBase from Control to UserControl

--- MVVMFirma/Models/Entities/Apteka.Designer.cs
+++ MVVMFirma/Models/Entities/Apteka.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Lukasz\OneDrive\Desktop\Dokumenty\Łukasz\Studia\3 rok\1 Semestr\Programowanie Desktopowych Aplikacji Biznesowych\Projekt\MVVMFirma\MVVMFirma\Models\Entities\Apteka.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Lukasz\OneDrive\Desktop\Dokumenty\Łukasz\Studia\3 rok\1 Semestr\Programowanie Desktopowych Aplikacji Biznesowych\Apteka\MVVMFirma\Models\Entities\Apteka.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
