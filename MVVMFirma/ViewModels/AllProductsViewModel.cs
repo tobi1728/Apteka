97ed74e0e05933d90a71fa8d9bb6dc4b0a0d3ade
Łukasz Tobolski <tobi1728@gmail.com>
2025-02-01 00:14:34 +0100

    Added last modal window. Project is ready for assessment.

--- MVVMFirma/ViewModels/AllProductsViewModel.cs
+++ MVVMFirma/ViewModels/AllProductsViewModel.cs
@@ -1,19 +1,18 @@
 ﻿using GalaSoft.MvvmLight.Messaging;
 using MVVMFirma.Helper;
+using MVVMFirma.Models.Entities;
 using MVVMFirma.Models.EntitiesForView;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 
 namespace MVVMFirma.ViewModels
 {
     public class AllProductsViewModel : AllViewModel<ProductForAllView>
     {
-        // --------------------------------------
-        // 1) Pola i właściwości do mechanizmu modalnego
-        // --------------------------------------
-
+        #region Pola do mechanizmu modalnego (wybór produktu)
         private ProductForAllView _selectedProduct;
         public ProductForAllView SelectedProduct
         {
@@ -23,41 +22,195 @@ namespace MVVMFirma.ViewModels
                 _selectedProduct = value;
                 OnPropertyChanged(() => SelectedProduct);
 
-                // Jeśli okno jest uruchomione w trybie modalnym i user wybrał wiersz
+                // Jeśli okno otwarte w trybie modalnym i user wybrał wiersz:
                 if (_selectedProduct != null && IsModal)
                 {
-                    // Wysyłamy wybrany produkt do VM, który nas otworzył (np. NewWarehouseViewModel)
                     Messenger.Default.Send(_selectedProduct);
-                    // Zamykamy okno
                     OnRequestClose();
                 }
             }
         }
 
         public bool IsModal { get; set; } = false;
+        #endregion
 
-        // --------------------------------------
-        // 2) Konstruktor
-        // --------------------------------------
+        #region Pola i właściwości do filtrowania
+
+        private decimal? _cenaZakupuOd;
+        public decimal? CenaZakupuOd
+        {
+            get => _cenaZakupuOd;
+            set
+            {
+                _cenaZakupuOd = value;
+                OnPropertyChanged(() => CenaZakupuOd);
+            }
+        }
+
+        private decimal? _cenaZakupuDo;
+        public decimal? CenaZakupuDo
+        {
+            get => _cenaZakupuDo;
+            set
+            {
+                _cenaZakupuDo = value;
+                OnPropertyChanged(() => CenaZakupuDo);
+            }
+        }
+
+        private decimal? _cenaSprzedazyOd;
+        public decimal? CenaSprzedazyOd
+        {
+            get => _cenaSprzedazyOd;
+            set
+            {
+                _cenaSprzedazyOd = value;
+                OnPropertyChanged(() => CenaSprzedazyOd);
+            }
+        }
+
+        private decimal? _cenaSprzedazyDo;
+        public decimal? CenaSprzedazyDo
+        {
+            get => _cenaSprzedazyDo;
+            set
+            {
+                _cenaSprzedazyDo = value;
+                OnPropertyChanged(() => CenaSprzedazyDo);
+            }
+        }
+
+        private bool _tylkoNaRecepte;
+        public bool TylkoNaRecepte
+        {
+            get => _tylkoNaRecepte;
+            set
+            {
+                _tylkoNaRecepte = value;
+                OnPropertyChanged(() => TylkoNaRecepte);
+            }
+        }
+
+        private bool _tylkoRefundacja;
+        public bool TylkoRefundacja
+        {
+            get => _tylkoRefundacja;
+            set
+            {
+                _tylkoRefundacja = value;
+                OnPropertyChanged(() => TylkoRefundacja);
+            }
+        }
+
+        private bool _tylkoPrzeterminowane;
+        public bool TylkoPrzeterminowane
+        {
+            get => _tylkoPrzeterminowane;
+            set
+            {
+                _tylkoPrzeterminowane = value;
+                OnPropertyChanged(() => TylkoPrzeterminowane);
+            }
+        }
+
+        #endregion
+
+        #region Pola i właściwości do statystyk (liczba, przeterminowane, średnie ceny)
+
+        private int _countOfProducts;
+        public int CountOfProducts
+        {
+            get => _countOfProducts;
+            set
+            {
+                _countOfProducts = value;
+                OnPropertyChanged(() => CountOfProducts);
+            }
+        }
+
+        private int _countExpired;
+        public int CountExpired
+        {
+            get => _countExpired;
+            set
+            {
+                _countExpired = value;
+                OnPropertyChanged(() => CountExpired);
+            }
+        }
+
+        private decimal _avgPurchasePrice;
+        public decimal AvgPurchasePrice
+        {
+            get => _avgPurchasePrice;
+            set
+            {
+                _avgPurchasePrice = value;
+                OnPropertyChanged(() => AvgPurchasePrice);
+            }
+        }
+
+        private decimal _avgSellPrice;
+        public decimal AvgSellPrice
+        {
+            get => _avgSellPrice;
+            set
+            {
+                _avgSellPrice = value;
+                OnPropertyChanged(() => AvgSellPrice);
+            }
+        }
+
+        #endregion
+
+        #region Komendy
+
+        private ICommand _filterCommand;
+        public ICommand FilterCommand
+        {
+            get
+            {
+                if (_filterCommand == null)
+                    _filterCommand = new BaseCommand(() => Filter());
+                return _filterCommand;
+            }
+        }
+
+        // Przykład komendy ExportCsv (opcjonalnie)
+        private ICommand _exportCsvCommand;
+        public ICommand ExportCsvCommand
+        {
+            get => _exportCsvCommand;
+            set
+            {
+                _exportCsvCommand = value;
+                OnPropertyChanged(() => ExportCsvCommand);
+            }
+        }
+
+        #endregion
+
+        #region Konstruktor
 
         public AllProductsViewModel()
             : base("Wszystkie leki")
         {
+            // Ewentualna komenda ExportCsv:
+            // ExportCsvCommand = new BaseCommand(() => ExportCsv());
         }
 
-        // --------------------------------------
-        // 3) Implementacja abstrakcyjnych metod z AllViewModel<T>
-        // --------------------------------------
+        #endregion
+
+        #region Implementacja abstrakcyjnych metod
 
         /// <summary>
         /// Wczytanie listy produktów z bazy.
         /// </summary>
         public override void Load()
         {
-            // Minimalna implementacja: np. wczytaj listę do List
-            // Jeżeli chcesz – zaimplementuj logikę analogiczną do starej wersji,
-            // np.:
-            List = new ObservableCollection<ProductForAllView>(
+            // Wczytanie z bazy do List
+            List = new ObservableCollection<ProductForAllView>
+            (
                 from product in aptekaEntities.Leki
                 select new ProductForAllView
                 {
@@ -73,6 +226,8 @@ namespace MVVMFirma.ViewModels
                     Refundacja = product.Refundacja
                 }
             );
+
+            UpdateStatistics(); // liczymy statystyki po wczytaniu
         }
 
         /// <summary>
@@ -110,6 +265,8 @@ namespace MVVMFirma.ViewModels
                     List.OrderBy(p => p.Data_Waznosci)
                 );
             }
+
+            UpdateStatistics(); // after sorting
         }
 
         /// <summary>
@@ -117,8 +274,7 @@ namespace MVVMFirma.ViewModels
         /// </summary>
         public override void Find()
         {
-            // Dla prostoty, minimalna implementacja:
-            // Przywracamy pierwotną listę
+            // Przywracamy pełną listę
             Load();
 
             if (FindField == "Nazwa")
@@ -142,6 +298,8 @@ namespace MVVMFirma.ViewModels
                         && p.Nazwa_Producenta.StartsWith(FindTextBox, StringComparison.OrdinalIgnoreCase))
                 );
             }
+
+            UpdateStatistics(); // after searching
         }
 
         /// <summary>
@@ -160,10 +318,84 @@ namespace MVVMFirma.ViewModels
             return new List<string> { "Nazwa", "Kategoria", "Producent" };
         }
 
-        // --------------------------------------
-        // 4) Ewentualne dodatkowe metody/komendy
-        //     (ExportCsvCommand czy FilterCommand) 
-        //     możesz tu zaimplementować wedle potrzeb.
-        // --------------------------------------
+        #endregion
+
+        #region Filter()
+        /// <summary>
+        /// Filtrowanie wg cen, checkboxów.
+        /// </summary>
+        private void Filter()
+        {
+            // Najpierw wczytujemy pełną listę (by odświeżyć)
+            Load();
+
+            // Konstruujemy listę z warunkami
+            var filtered = List.AsEnumerable();
+
+            // Cena zakupu od/do
+            if (CenaZakupuOd.HasValue)
+                filtered = filtered.Where(p => p.Cena_Zakupu >= CenaZakupuOd.Value);
+
+            if (CenaZakupuDo.HasValue)
+                filtered = filtered.Where(p => p.Cena_Zakupu <= CenaZakupuDo.Value);
+
+            // Cena sprzedaży od/do
+            if (CenaSprzedazyOd.HasValue)
+                filtered = filtered.Where(p => p.Cena_Sprzedaży >= CenaSprzedazyOd.Value);
+
+            if (CenaSprzedazyDo.HasValue)
+                filtered = filtered.Where(p => p.Cena_Sprzedaży <= CenaSprzedazyDo.Value);
+
+            // TylkoNaRecepte
+            if (TylkoNaRecepte)
+                filtered = filtered.Where(p => p.Na_Recepte);
+
+            // TylkoRefundacja
+            if (TylkoRefundacja)
+                filtered = filtered.Where(p => p.Refundacja);
+
+            // TylkoPrzeterminowane
+            if (TylkoPrzeterminowane)
+                filtered = filtered.Where(p => p.Data_Waznosci < DateTime.Today);
+
+            // Na koniec przypisujemy do List
+            List = new ObservableCollection<ProductForAllView>(filtered.ToList());
+
+            UpdateStatistics();
+        }
+        #endregion
+
+        #region UpdateStatistics()
+        /// <summary>
+        /// Liczenie liczby produktów, liczby przeterminowanych, średnich cen.
+        /// </summary>
+        private void UpdateStatistics()
+        {
+            if (List == null || List.Count == 0)
+            {
+                CountOfProducts = 0;
+                CountExpired = 0;
+                AvgPurchasePrice = 0;
+                AvgSellPrice = 0;
+                return;
+            }
+
+            CountOfProducts = List.Count;
+            // Przeterminowane => Data_Waznosci < DateTime.Today
+            CountExpired = List.Count(p => p.Data_Waznosci < DateTime.Today);
+
+            AvgPurchasePrice = Math.Round((decimal)List.Average(p => p.Cena_Zakupu), 2);
+            AvgSellPrice = Math.Round((decimal)List.Average(p => p.Cena_Sprzedaży), 2);
+        }
+        #endregion
+
+        #region Ewentualna metoda ExportCsv (opcjonalnie)
+        /*
+        private void ExportCsv()
+        {
+            // ...
+        }
+        */
+        #endregion
     }
 }
